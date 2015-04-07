c55361fab84ac6873d6c16137f06834dcd1493f3
Blackflunk <Alexander12350@hotmail.com>
2015-04-07 16:52:50 +0200

    Socket og implementeret

--- 12_DATAKOM2/src/ftp/FTPClient.java
+++ 12_DATAKOM2/src/ftp/FTPClient.java
@@ -1,5 +1,18 @@
 package ftp;
+import java.net.Socket;
 
-public class FTPClient {
+public class FTPClient implements IFTPClient {
+Socket sock;
+
+	@Override
+	public void LIST() {
+		
+	}
+
+	@Override
+	public void RETR() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
