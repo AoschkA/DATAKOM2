49927ccf873289ac6e20e703729f5d06626c4b85
Andreas-Strange <andreas_strange@hotmail.com>
2015-04-08 12:49:18 +0200

    gh

--- 12_DATAKOM2/src/Launch.java
+++ 12_DATAKOM2/src/Launch.java
@@ -1,5 +1,14 @@
+import wcu.FileHandler;
+
 
 public class Launch {
+//	public static void main(String[] args) {
+//		FileHandler fh = new FileHandler();
+//		fh.showReadFile();
+		
+//	}
+	
+	
 	
 	
 
