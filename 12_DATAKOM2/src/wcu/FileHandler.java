844fd7f9083bfbb50dc78b03e9bac19f44e80695
Andreas-Strange <andreas_strange@hotmail.com>
2015-04-09 17:12:58 +0200

    tilføjet checkIfOperatoerExists(), + read og write til begge tekstfiler

--- 12_DATAKOM2/src/wcu/FileHandler.java
+++ 12_DATAKOM2/src/wcu/FileHandler.java
@@ -4,71 +4,85 @@ import java.io.IOException;
 
 public class FileHandler {
 
-	//	String kun til test
-	//	String textfile = "something";
 
-	//	Her defineres stien der skal læses fra/ eller skrives til.		
-	String textfile = "C:/Users/Andreas/git/DATAKOM2/12_DATAKOM2/src/wcu/store.txt";
+	//	Her defineres stien der skal læses fra/ eller skrives til.	
+
+	String operatoerTextFile = "data.text/operatoer.txt";
+	String storeTextFile = "data.text/store.txt";
 
 	public FileHandler() {
 	}
 
-	public void showReadFile(){
+	public void readOperatoerFile(){
 
 		try{
-			ReadFile file = new ReadFile(textfile);
+			ReadFile file = new ReadFile(operatoerTextFile);
 			String[] arraytext = file.OpenFile();
-			int sc = 1;
+
 			for (int i = 0; i < arraytext.length; i++){
-				//				System.out.println(arraytext[i]);
-				String word = arraytext[i];
-				String [] wordsinline = word.split(",");
-
-
-				if (sc == Integer.parseInt(wordsinline[0])) {
-					System.out.println("Godkendt");
-
-				}
-				else {
-					System.out.println("Findes ikke");
-				}
+				System.out.println(arraytext[i]);
 			}
 		}
 		catch (IOException e){
 			System.out.println(e.getMessage());
 		}
 	}
-	public void executeWriteFile(){
+	public void readStoreFile(){
 
 		try{
-			WriteFile data = new WriteFile(textfile, true);
+			ReadFile file = new ReadFile(storeTextFile);
+			String[] arraytext = file.OpenFile();
+
+			for (int i = 0; i < arraytext.length; i++){
+				System.out.println(arraytext[i]);
+			}
+		}
+		catch (IOException e){
+			System.out.println(e.getMessage());
+		}
+	}
+	public void writeToOperatoerFile(){
+
+		try{
+			WriteFile data = new WriteFile(operatoerTextFile, true);
 
 			//		Her skrives teksten der skal gemmes
 			data.writeTofileFile("OST");
 
-			//			Her defineres stien der skal skrive til:			
-			//			System.out.println("C:/Users/Andreas/workspace/Datakommunikation/ReadFile/src/store.txt");
+		}
+		catch (IOException e){
+			System.out.println(e.getMessage());
+		}
+	}
+	public void writeToStoreFile(){
+
+		try{
+			WriteFile data = new WriteFile(storeTextFile, true);
+
+			//		Her skrives teksten der skal gemmes
+			data.writeTofileFile("OST");
+
 		}
 		catch (IOException e){
 			System.out.println(e.getMessage());
 		}
 	}
 	public void checkIfOperatorExists(){
-		
+
 		boolean exists = false;
 		try{
-			ReadFile file = new ReadFile(textfile);
+			ReadFile file = new ReadFile(operatoerTextFile);
 			String[] arraytext = file.OpenFile();
 			int count = 0;
-			int sc = 15;
+			int sc = 10231;
 			for (int i = 0; i < arraytext.length; i++){
 				String word = arraytext[i];
 				String [] wordsinline = word.split(",");
 
 				if (sc == Integer.parseInt(wordsinline[0])) {
 					count ++;
-								}
-							}
+				}
+			}
 			if(count==1){
 				exists = true;
 				System.out.println("Operatoer Godkendt");
