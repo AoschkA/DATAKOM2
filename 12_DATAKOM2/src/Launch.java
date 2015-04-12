79e74fd62d301fc653780677e1a969ae959daaf9
ZeenGoone <zeengoone@gmail.com>
2015-04-12 19:10:29 +0200

    valueændring

--- 12_DATAKOM2/src/Launch.java
+++ 12_DATAKOM2/src/Launch.java
@@ -14,17 +14,26 @@ public class Launch {
 		IOController io = new IOController();
 		vareDB = io.readVareFil();
 		oprDB = io.readOperatoerFil();
+		int rigtigvare = 0;
+		int rigtigoperatoer = 0;
+		double vareaendring = 0.12;
 		for(int i = 0;i < vareDB.size();i++){
 			if(vareDB.get(i).getRaavareNr() == 8) {
 				System.out.println(vareDB.get(i).getRaavareNr() +" "+ vareDB.get(i).getRaavareNavn() +" "+ vareDB.get(i).getRaavareWeight());
+				rigtigvare = i;
 			}
 		}
 		for(int i = 0;i < oprDB.size();i++){
 			if(oprDB.get(i).getOprNr() == 42352) {
 				System.out.println(oprDB.get(i).getOprNr() +" "+ oprDB.get(i).getOprName());
+				rigtigoperatoer = i;
 			}
 		}
+		System.out.println("Vare fundet på plads " + rigtigvare);
+		System.out.println("Operatør fundet på plads " + rigtigoperatoer);
 		
+		System.out.println("Så lad os ændre vægten på den vare: ");
+		System.out.println(Double.parseDouble(vareDB.get(rigtigvare).getRaavareWeight())-vareaendring);
 	}
 	
 }
