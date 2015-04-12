d05de8a4e9af0f820e7f2d6292597858f8a5859e
ZeenGoone <zeengoone@gmail.com>
2015-04-12 18:40:00 +0200

    Indført et databaseobjekt, og lavet et eksempel på at hive alt fra en fil ind i en varedatabase som der så kan testes på :)

--- 12_DATAKOM2/src/Launch.java
+++ 12_DATAKOM2/src/Launch.java
@@ -1,10 +1,20 @@
-import wcu.FileHandler;
+//import wcu.FileHandler;
+import java.util.ArrayList;
+
+import wcu.Database;
+import wcu.IOController;
 
 
 public class Launch {
 	public static void main(String[] args) {
-		FileHandler fh = new FileHandler();
-		fh.checkIfIngredientExists();
+		//FileHandler fh = new FileHandler();
+		//fh.checkIfIngredientExists();
+		ArrayList<Database> vareDB = new ArrayList<Database>();
+		IOController io = new IOController();
+		vareDB = io.readVareFil();
+		for(int i = 0;i < vareDB.size();i++){
+			System.out.println(vareDB.get(i).getRaavareNr() +" "+ vareDB.get(i).getRaavareNavn() +" "+ vareDB.get(i).getRaavareWeight());
+		}
 		
 	}
 	
--- 12_DATAKOM2/src/wcu/IOController.java
+++ 12_DATAKOM2/src/wcu/IOController.java
@@ -1,9 +1,35 @@
 package wcu;
 
-public class IOController {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 
+public class IOController {
+	String csv_Character = ",";
+	String current_Database_Fil = "data.text/store.txt";
+	BufferedReader br = null;
+	String linje = "";
+	
 	public IOController() {
 		// TODO Auto-generated constructor stub
 	}
-
+	public ArrayList<Database> readVareFil(){
+		
+		Database nyBasePost;
+		ArrayList<Database> nyBase = new ArrayList<Database>();
+		try {
+			br = new BufferedReader(new FileReader(current_Database_Fil));
+			while((linje = br.readLine()) != null){
+				String[] tempDB = linje.split(csv_Character);
+				nyBasePost = new Database();
+				nyBasePost.setRaavareNr(Integer.parseInt(tempDB[0]));
+				nyBasePost.setRaavareNavn(tempDB[1]);
+				nyBasePost.setRaavareWeight(tempDB[2]);
+				nyBase.add(nyBasePost);
+			}	
+		}catch(Exception e){
+			System.out.println("Show me I work!");
+		}
+		return nyBase;
+	}
 }
