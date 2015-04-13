d2eff38f189fa9a1f824d7f143a2c46d3c8c194e
Andreas-Strange <andreas_strange@hotmail.com>
2015-04-13 20:58:52 +0200

    OperatoerControl lavet

--- 12_DATAKOM2/src_wcu/functionality/OperatoerDAO.java
+++ 12_DATAKOM2/src_wcu/functionality/OperatoerDAO.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public interface OperatoerDAO {
 	public OperatoerDTO getOperatoer(int oprID);
-	public void updateOperatoer(int oprID);
+	public void updateOperatoer(OperatoerDTO oprID);
 	public void createOperatoer(ArrayList<OperatoerDTO> oprDB, OperatoerDTO opr);
 	public void deleteOperatoer(int oprID);
 	public ArrayList<OperatoerDTO> getOperatoerList();
--- 12_DATAKOM2/src_wcu/functionality/OprControl.java
+++ 12_DATAKOM2/src_wcu/functionality/OprControl.java
@@ -4,38 +4,54 @@ import java.util.ArrayList;
 
 public class OprControl implements OperatoerDAO{
 
+	FileHandler operatoerFil;
+	ArrayList<OperatoerDTO> oDB = new ArrayList<OperatoerDTO>();
+
 	public OprControl() {
-		
+
+		operatoerFil = new FileHandler();
+		oDB = operatoerFil.readOperatoerDB();
+
 	}
 
 	@Override
 	public OperatoerDTO getOperatoer(int oprID) {
-		// TODO Auto-generated method stub
-		return null;
+		OperatoerDTO hentOperatoer = new OperatoerDTO();
+		for (int i = 0; i < oDB.size(); i ++){
+			if (oprID == oDB.get(i).getOprID()){
+				return oDB.get(i);
+			}
+		}
+		hentOperatoer.setOprID(999999);
+		return hentOperatoer;
 	}
 
 	@Override
-	public void updateOperatoer(int oprID) {
-		// TODO Auto-generated method stub
-		
+	public void updateOperatoer(OperatoerDTO oprID) {
+		for(int i = 0; i < oDB.size(); i++){
+			if(oprID.getOprID() == oDB.get(i).getOprID()){
+				oDB.set(i, oprID);
+			}
+		}
+		operatoerFil.writeOperatoerDB(oDB);
 	}
 
+	@Override
+	public ArrayList<OperatoerDTO> getOperatoerList() {
+		return oDB;
+	}
 	@Override
 	public void createOperatoer(ArrayList<OperatoerDTO> oprDB, OperatoerDTO opr) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteOperatoer(int oprID) {
 		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public ArrayList<OperatoerDTO> getOperatoerList() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
+
+
