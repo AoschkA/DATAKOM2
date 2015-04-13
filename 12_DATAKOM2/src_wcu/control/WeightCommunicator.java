0a2850a15d69e9faf84e3ed90adeaab8d5441a8b
teebse <Teebanaser@10.16.172.224>
2015-04-13 12:57:59 +0200

    vægt connector

--- 12_DATAKOM2/src_wcu/control/WeightCommunicator.java
+++ 12_DATAKOM2/src_wcu/control/WeightCommunicator.java
@@ -1,7 +1,24 @@
 package control;
 
-public class WeightCommunicator {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
+public class WeightCommunicator {
+	// opret forbindelse til vægt
+	private int portdst = 8000;
+	private Socket sock ;
+	private BufferedReader instream ; 
+	private DataOutputStream outstream ; 
+	private int opr_nr , raavare_nr, raavare_weight; 
+	private String raavare_navn, opr_name,
+	filenameStore = "store.txt",
+	logfile = "log.txt";
+
+	public void run ( int port ) {
+		
+	}
+	
 	public WeightCommunicator() {
 		// TODO Auto-generated constructor stub
 	}
