80d5c4e1559f81d13d0f51ea2abf08967dd95223
Andreas-Strange <andreas_strange@hotmail.com>
2015-04-13 22:52:57 +0200

    lidt mere

--- 12_DATAKOM2/src_wcu/functionality/IFileHandler.java
+++ 12_DATAKOM2/src_wcu/functionality/IFileHandler.java
@@ -6,7 +6,9 @@ public interface IFileHandler {
 
 	ArrayList<OperatoerDTO> readOperatoerDB();
 	ArrayList<RaavareDTO> readRaavareDB();
+	ArrayList<LogDTO> readLogDB();
 	void writeOperatoerDB(ArrayList<OperatoerDTO> oprDB);
 	void writeRaavareDB(ArrayList<RaavareDTO> rDB);
+	void writeLogDB(ArrayList<LogDTO> logDB);
 
 }
--- 12_DATAKOM2/src_wcu/functionality/LogDAO.java
+++ 12_DATAKOM2/src_wcu/functionality/LogDAO.java
@@ -1,5 +1,11 @@
 package functionality;
 
+import java.util.ArrayList;
+
 public interface LogDAO {
+	
+	public void registerLogOn(OperatoerDTO oprID);
+	public void registerAfvejning(RaavareDTO raavareWeight);
+	public ArrayList<LogDTO> getLogList();
 
 }
