1a6859a146223277db4d43ebb2b4aab900c1a0d6
ZeenGoone <Charles@TheDen.win.dtu.dk>
2015-04-14 16:05:21 +0200

    statemachine in progress

--- 12_DATAKOM2/src_wcu/functionality/UserHandler.java
+++ 12_DATAKOM2/src_wcu/functionality/UserHandler.java
@@ -5,6 +5,8 @@ import control.WeightCommunicator;
 public class UserHandler {
 	public static WeightCommunicator wc = new WeightCommunicator("127.0.0.1",
 			4567);
+	public static OprControl oc = new OprControl();
+	public static RaavareControl rc = new RaavareControl();
 	public static boolean run = true;
 	public static String vaegtSvar = "";
 	int x;
@@ -18,11 +20,13 @@ public class UserHandler {
 	public enum State {
 		START {
 			@Override
-			State changeState(int x) {
-				switch (x) {
-				case 10:
+			State changeState(String svar) {
+				switch (svar) {
+				case "ja":
 					vaegtSvar = wc
-							.writeSocket("RM20 8 \"Indtast operatørID\" \"3\" \"hej\" \r\n");
+							.writeSocket("RM20 8 \"OperatoerID?\" \" \" \" \" \r\n");
+					vaegtSvar = wc.readSocket();
+					vaegtSvar = vaegtSvar.substring(7, vaegtSvar.length());
 					return STATE1;
 				default:
 					return INVALIDSTATE;
@@ -32,19 +36,142 @@ public class UserHandler {
 		STATE1 {
 
 			@Override
-			State changeState(int x) {
-				if (vaegtSvar.equals("RM20 B")) {
-					System.out.println("hejsa");
+			State changeState(String svar) {
+				
+				switch (svar) {
+				case "ja":
+					vaegtSvar = wc.writeSocket("RM20 8 \"Varenummer?\" \" \" \" \" \r\n");
+					vaegtSvar = wc.readSocket();
+					vaegtSvar = vaegtSvar.substring(7, vaegtSvar.length());
+					return STATE2;
+				case "nej":
+					return START;
+				case "Q":
+					return STOP;
+				default:
+					return INVALIDSTATE;
 				}
-				switch (x) {
-				case 12:
-					System.out.println("I am now changing from state " + this
-							+ " with int argument x = " + x);
-					System.out.println(wc.readSocket());
+			}
+		},
+		STATE2 {
+
+			@Override
+			State changeState(String svar) {
+				
+				switch (svar) {
+				case "ja":
+					vaegtSvar = wc.writeSocket("RM20 8 \"" + rc.getRaavare(Integer.parseInt(vaegtSvar)).getRaavareName() + "?\"\"ja \" \" nej\" \r\n");
+					vaegtSvar = wc.readSocket();
+					vaegtSvar = vaegtSvar.substring(7, vaegtSvar.length());
+					return STATE3;
+				case "Q":
+					return STOP;
+				default:
+					return INVALIDSTATE;
+				}
+			}
+		},
+		STATE3 {
+
+			@Override
+			State changeState(String svar) {
+				
+				switch (svar) {
+				case "ja":
+					System.out.println("KLAPHAT");
+					return STATE4;
+				case "Q":
+					return STOP;
+				default:
+					return INVALIDSTATE;
+				}
+			}
+		},		
+		STATE4 {
+
+			@Override
+			State changeState(String svar) {
+				
+				switch (svar) {
+				case "ja":
+					System.out.println("State 4!");
+					return STATE5;
+				case "Q":
+					return STOP;
+				default:
+					return INVALIDSTATE;
+				}
+			}
+		},
+		STATE5 {
+
+			@Override
+			State changeState(String svar) {
+				
+				switch (svar) {
+				case "ja":
+					return STATE6;
+				case "Q":
+					return STOP;
+				default:
+					return INVALIDSTATE;
+				}
+			}
+		},		
+		STATE6 {
+
+			@Override
+			State changeState(String svar) {
+				
+				switch (svar) {
+				case "ja":
+					return STATE7;
+				case "Q":
+					return STOP;
+				default:
+					return INVALIDSTATE;
+				}
+			}
+		},
+		STATE7 {
+
+			@Override
+			State changeState(String svar) {
+				
+				switch (svar) {
+				case "ja":
+					return STATE8;
+				case "Q":
+					return STOP;
+				default:
+					return INVALIDSTATE;
+				}
+			}
+		},
+		STATE8 {
+
+			@Override
+			State changeState(String svar) {
+				
+				switch (svar) {
+				case "ja":
+					return STATE9;
+				case "Q":
+					return STOP;
+				default:
+					return INVALIDSTATE;
+				}
+			}
+		},
+		STATE9 {
+
+			@Override
+			State changeState(String svar) {
+				
+				switch (svar) {
+				case "ja":
 					return START;
-				case -1:
-					return START;
-				case -2:
+				case "Q":
 					return STOP;
 				default:
 					return INVALIDSTATE;
@@ -54,34 +181,44 @@ public class UserHandler {
 		INVALIDSTATE {
 
 			@Override
-			State changeState(int x) {
-				System.out.println("invalidstate");
+			State changeState(String svar) {
+				System.out.println("invalidstate " + svar);
 				return STOP;
 			}
 		},
 		STOP {
 
 			@Override
-			State changeState(int x) {
+			State changeState(String svar) {
 				return STOP;
 			}
 		};
-		abstract State changeState(int x);
+		abstract State changeState(String svar);
 	}
 
 	private State state;
 
-	public void runScheme(int x) {
-		this.state = this.state.changeState(x);
+	public void runScheme(String svar) {
+		this.state = this.state.changeState(svar);
 	}
 
 	public static void main(String[] args) {
 		// Start af kontrolsekvens
-		System.out.println("Sekvensmaskinen startet.");
 		UserHandler u = new UserHandler();
 		try {
-			u.runScheme(10);
-			u.runScheme(12);
+			u.runScheme("ja");
+			System.out.println(vaegtSvar);
+			if(oc.getOperatoer(Integer.parseInt(vaegtSvar)).getOprID() != 999999){
+				u.runScheme("ja");
+			}
+			System.out.println(vaegtSvar);
+			if(rc.getRaavare(Integer.parseInt(vaegtSvar)).getRaavareID() != 999999) {
+				u.runScheme("ja");
+			}
+			System.out.println(vaegtSvar);
+			if(vaegtSvar.equals("ja")){
+				u.runScheme("ja");
+			}
 		} catch (Exception e) {
 			System.out.println("intet data på port");
 		}
